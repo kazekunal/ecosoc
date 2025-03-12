102ba1d0f60bdd0eae048a50c0668e3f943c03ba
kunal passan <Kunalpassan30@gmail.com>
2025-03-12 13:06:46 +0530

    contact

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,13 @@
+import { Contact } from 'lucide-react'
+import React from 'react'
+import ContactUs from '../components/contact'
+
+function page() {
+  return (
+    <div>
+        <ContactUs/>
+    </div>
+  )
+}
+
+export default page
